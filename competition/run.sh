07f9aacc962ea65856f3b3ae308c3c1c2a6cb5cd
Thimon Pelka <thimon.pelka@gmail.com>
2025-06-13 11:46:58 +0200

    fix: updated run sh to install tqdm

--- competition/run.sh
+++ competition/run.sh
@@ -8,10 +8,12 @@ fi
 
 PCAP_FILE="$1"
 
+python -m pip install tqdm
+
 chmod +x go-flows
 # 1. Run go-flows to extract features from the PCAP file
 ./go-flows run features 4tuple_bidi.json export csv test.csv source libpcap "$PCAP_FILE"
 
 # 2. Run prediction script
 # python predict.py
-python predict_enhanced.py -m enhanced_threat_detection_model -i test.csv -o output.csv
+python predict_enhanced.py -m enhanced_threat_detection_model.pkl -i test.csv -o output.csv
